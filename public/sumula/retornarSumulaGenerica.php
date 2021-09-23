d2d0051396be217264f43a2c0dcab4a1778d0138
DEA_ASILVA <4ahwir2oiaksdyfpqtmfiqjw2pqz6hkzdhdt3dnjtg5dob5sxzvq>
2021-09-23 13:01:35 -0300

    aaa

--- public/sumula/retornarSumulaGenerica.php
+++ public/sumula/retornarSumulaGenerica.php
@@ -350,8 +350,13 @@ while ($campo = mysqli_fetch_row($sqlTimes)) {
   array_push($arrayAtletas2, $arrayAtleta2);
 }
 
+$dataJogo = $data[8] . $data[9] . "/" . $data[5] . $data[6] . "/" . $data[0] . $data[1] . $data[2] . $data[3];
+
+$dataPartida = $dataJogo." " . $horaJogo;
+
+
 $arraySimples = array(
-  "dataPartida" => $data,
+  "dataPartida" => $dataPartida,
   "rodada" => $rodada
 );
 
